691c6c4715af05aca50ed2c5a037627ad431dcb4
auifzysr <auifzysr@gmail.com>
2022-05-07 16:48:03 +0900

    run as service account

--- tf/main.tf
+++ tf/main.tf
@@ -16,6 +16,36 @@ resource "google_project_service" "eventarc" {
   service = "eventarc.googleapis.com"
 }
 
+resource "google_service_account" "trigger-audit" {
+  account_id   = local.resource_name
+  display_name = local.resource_name
+}
+
+resource "google_project_iam_binding" "trigger-audit" {
+  project = data.google_project.project.project_id
+  role    = "roles/eventarc.eventReceiver"
+  members = [
+    "serviceAccount:${google_service_account.trigger-audit.email}"
+  ]
+
+  depends_on = [
+    google_service_account.trigger-audit
+  ]
+}
+
+# TODO: restrict to specific service
+resource "google_project_iam_binding" "invoke-cloudrun" {
+  project = data.google_project.project.project_id
+  role    = "roles/run.invoker"
+  members = [
+    "serviceAccount:${google_service_account.trigger-audit.email}"
+  ]
+
+  depends_on = [
+    google_service_account.trigger-audit
+  ]
+}
+
 resource "google_eventarc_trigger" "trigger-audit" {
   name     = local.resource_name
   location = var.region
@@ -30,8 +60,10 @@ resource "google_eventarc_trigger" "trigger-audit" {
       region  = var.region
     }
   }
+  service_account = google_service_account.trigger-audit.email
 
   depends_on = [
-    google_project_service.eventarc
+    google_project_service.eventarc,
+    google_service_account.trigger-audit
   ]
 }
